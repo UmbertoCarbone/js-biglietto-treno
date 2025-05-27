4eab4effcca9cf9656e850465f540a7286902c31
UmbertoCarbone <umberto.carb.dev@gmail.com>
2025-05-27 15:01:21 +0200

    Suddivisione

--- treno.js
+++ treno.js
@@ -1,1 +1,28 @@
-console.log("ciao")
+
+
+
+/*il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+il prezzo del biglietto è definito in base ai km(0.21 € al km)
+va applicato uno sconto del 20 % per i minorenni
+va applicato uno sconto del 40 % per gli over 65.
+L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
+
+
+Questo richiederà un minimo di ricerca.
+TIP: per controllare che la vostra logica sui prezzi funzioni correttamente, provate a verificare quanto segue:
+
+100km, 10 anni => prezzo corretto: €16.80
+100km, 70 anni => prezzo corretto: €12.60 */
+
+// 1 - Chiedere all'utente quanti km vuole percorrere
+// 2 - Chiedere l'età del passeggero
+// 3 - Salvo i dati in variabili
+// 4 - Calcolare il prezzo con 2 decimali
+// 5 - Verificare l'età per applicare eventuali sconti
+// 6 - Applicare sconto 20% per minorenni
+// 7 - Applicare sconto 40% per over 65
+// 8 - Output prezzo con massimo due decimali
+
+
